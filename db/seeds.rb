249fbe055c86287ed7e420c79380650afa087b7c
kyrylo_teplynskyi <kyryloteplynskyi@gmail.com>
2023-05-21 14:06:08 +0300

    added seeds for comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,16 @@ if posts.empty?
   user_rel = User.find_by(id: 1)
   Post.create!(title: "test post title 1", excerpt: "test post excerpt 1", content: "<p>text post content 1</p>", user_id: user_rel.id)
 end
+
+comments = Comment.all
+if comments.empty?
+  user_rel = User.first
+  post_rel = Post.first
+  
+  Comment.create(body: "test comment, no parent", user_id: user_rel.id, post_id: post_rel.id)
+else 
+  comment_rel = Comment.first
+  user_rel = User.first
+  post_rel = Post.first
+  Comment.create(body: "text child comment", user_id: user_rel.id, post_id: post_rel.id, parent_id: comment_rel.id)
+end
