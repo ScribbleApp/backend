aed09ca77fba3acd3c16c9b8151f7319d14166e0
ruslan <morozov2020ks11@student.karazin.ua>
2023-05-23 13:16:07 +0300

    ability to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:create]
   def index
     posts = Post.all.order(:created_at)
     posts = posts.map { | post |
@@ -15,4 +15,17 @@ class PostsController < ApplicationController
     render json: {id:post.id, title:post.title, excerpt:post.excerpt, content:post.content, createdAt:post.created_at, user:{id:user.id, email:user.email, name:user.name}}
   end
 
+  # excerpt: "test post excerpt 2", content: "<p>text post content 2</p>", user_id: user_rel.id
+  def create
+    new_post = Post.create(post_params)
+    render json: new_post, status: :ok
+  end
+
+  # post_params, user: current_user.id
+  private
+  def post_params
+    new_post = params.require(:post).permit(:title, :excerpt, :content)
+    new_post['user_id'] = current_user.id
+    new_post
+  end
 end
